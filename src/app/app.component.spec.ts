7be29ef07dff5d26e284ea7690d208cd8d0a8836
kLeitich <kevleitich@gmail.com>
2022-04-17 19:32:11 +0300

    adding files on repo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'QuoteApp'`, () => {
+  it(`should have as title 'Quoteapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('QuoteApp');
+    expect(app.title).toEqual('Quoteapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('QuoteApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Quoteapp app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuoteComponent } from './quote/quote.component';
-import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QouteComponent } from './qoute/qoute.component';
+import { QouteDetailsComponent } from './qoute-details/qoute-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent,
-    QuoteDetailsComponent
+    QouteComponent,
+    QouteDetailsComponent
   ],
   imports: [
     BrowserModule,
