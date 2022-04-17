268c219eb1035988f4e9100a574c34a46aae5e8c
kLeitich <kevleitich@gmail.com>
2022-04-17 19:29:03 +0300

    addding qoute class

---
+++ src/app/quote.ts
@@ -0,0 +1,10 @@
+export class Quote {
+    showDescription: boolean;
+    constructor(public description:string,public author:string,public postBy:string){
+        this.showDescription=false;
+    }
+    
+    
+    
+
+}
