1543cf02689a83f4c8c230cef6fdbe3f72706ec2
kLeitich <kevleitich@gmail.com>
2022-04-19 07:21:43 +0300

    adding a functioning time pipe

--- src/app/qoute-form/qoute-form.component.ts
+++ src/app/qoute-form/qoute-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Quote } from '../quote';
+import { DatepostedPipe } from '../dateposted.pipe';
 
 @Component({
   selector: 'app-qoute-form',
@@ -9,10 +10,14 @@ import { Quote } from '../quote';
 export class QouteFormComponent implements OnInit {
   
   newQuote= new Quote("","","");
+  newDate = new Date();
 @Output() addQoute = new EventEmitter<Quote>();
+@Output() addTime = new EventEmitter<Date>();
 
   submitQoute(){
     this.addQoute.emit(this.newQuote);
+    this.addTime.emit(this.newDate)
+    
   }
   constructor() { }
 
